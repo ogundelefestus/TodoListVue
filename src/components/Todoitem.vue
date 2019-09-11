a94bb908c62a447b1d61fe7ccb784aa9e419014a
festusogundele <festusogundele9@gmail.com>
2019-09-11 06:08:04 +0100

    my first vue js app

---
+++ src/components/Todoitem.vue
@@ -0,0 +1,59 @@
+<template>
+ <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+
+ 	<button @click="$emit('del-todo',todo.id) " class="del">x</button>
+<p>
+
+<input type="checkbox" v-on:change="markComlete">
+
+
+{{todo.title}}</p>
+
+
+ </div>
+
+</template>
+<script >
+	export default{ 
+
+		name: "Todoitem",
+		props:["todo"],
+
+		methods: {
+		markComlete(){
+
+			this.todo.completed=!this.todo.completed;
+		}
+
+	}
+}
+
+</script>
+
+<style scoped>
+	.todo-item{
+
+		background:#f4f4f4;
+		padding:10px;
+		border-bottom: 1px #ccc dotted;
+	}
+    .is-complete{
+
+
+    	text-decoration: line-through;
+    }
+    .del{
+
+
+    	background: #ff0000;
+    	color: #fff;
+    	border:none;
+    	padding:5px 9px;
+    	border-radius: 50%;
+    	cursor: pointer;;
+    	float: right;
+    }
+
+
+
+</style>
